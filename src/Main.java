a40fd5261d35bd26574b83b6b55e0095a2434590
irina p <gorohovairina21@gmail.com>
2023-03-13 09:57:01 +0300

    change Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-       int startSum = 100;
-       int deposit = 1000;
-       int bonus;
-       int finalSum;
+        int startSum = 100;
+        int deposit = 1000;
+        int bonus;
+        int finalSum;
 
-       if (deposit >= 1000) {
-           finalSum = deposit / 100 + deposit + startSum; bonus = deposit / 100;
-       } else {
-           finalSum = deposit + startSum; bonus = 0;
-       }
+        if (deposit >= 1000) {
+            finalSum = deposit / 100 + deposit + startSum;
+            bonus = deposit / 100;
+        } else {
+            finalSum = deposit + startSum;
+            bonus = 0;
+        }
         System.out.println("Итоговый счет " + finalSum);
         System.out.println("Бонусный счет " + bonus);
     }
